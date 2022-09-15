b459e292ffcf656669a1086cc85a32007f51489d
OLIVER RAÚL VELÁZQUEZ TORRES <oliver885@hotmail.com>
2022-09-15 14:28:21 -0500

    Plantilla Uno 15092022

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>XVIII Congreso Nacional de Administración y Negocios 2022</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 m-0 p-0">
+
+                <!-- Menu -->
+                <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+
+                        <a class="navbar-brand" href="#">
+                            <img src="img/Logo.svg" height="50" class="d-inline-block align-top mx-5 animate__animated animate__fadeInLeft" alt="Logo">
+                        </a>
+                
+                        <button class="navbar-toggler mx-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                    
+                        <div class="collapse navbar-collapse mx-3" id="navbarSupportedContent">
+                            <ul class="navbar-nav mx-auto d-flex justify-content-center">
+                                <li class="nav-item">
+                                    <a class="nav-link active" href="#">Inicio</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Conferencias</a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="#">Magistrales</a></li>
+                                        <li><a class="dropdown-item" href="#">Temáticas</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Foros</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Concursos</a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="#">Emprendedores</a></li>
+                                        <li><a class="dropdown-item" href="#">Simuladores</a></li>
+                                    </ul>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Actividades</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Talleres</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Iniciar sesión</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Registro</a>
+                                </li>
+                        </ul>
+                    </div>
+                </nav>
+                <!-- Menu -->
+
+                <!-- header -->
+                <header class="mt-3">
+                    <div class="overlay"></div>
+                    <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop" class="animate__animated animate__fadeIn">
+                      <source src="videos/VideoIntroWeb.mp4" type="video/mp4">
+                    </video>
+                    <div class="container h-100">
+
+                      <div class="d-flex h-100 text-center align-items-center">
+
+                        <div class="w-100 text-white">
+                          <img src="img/logoVideo.svg" class="img-fluid mx-auto mb-5 animate__animated animate__fadeIn animate__delay-1s" style="width: 300px;" alt="XVIII Congreso">
+                          <p class="mb-0 animate__animated animate__fadeIn animate__delay-1s">La Dirección General de Universidades Tecnológicas y Politécnicas a través de la Universidad Tecnológica Fidel Velázquez convoca al:<p>
+                          <h4 class="mb-0 text-warning animate__animated animate__fadeIn animate__delay-1s">“Retos y perspectivas del Mundo de la Administración Pos-pandemia”<h4>
+                          <h1 class="animated text-warning animate__animated animate__fadeIn animate__delay-1s">16, 17 y 18 de Noviembre</h1>
+                        </div>
+
+                      </div>
+
+                    </div>
+                  </header>
+                <!-- header -->
+
+            </div>
+        </div>
+    </div>
+    <!-- <script src="js/jquery.min.js"></script> -->
+    <!-- <script src="js/bootstrap.min.js"></script> -->
+    <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
