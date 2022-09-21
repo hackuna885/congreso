c3e962e72fc6cdcd6ba5f3cb4bd91cb978538099
hackuna hack <oliver885@hotmail.com>
2022-09-20 19:46:22 -0500

    20092022

--- index.php
+++ index.php
@@ -89,10 +89,193 @@
                 <!-- header -->
 
             </div>
+            <div class="row">
+
+                <div class="col-8 my-5 mx-auto">
+                    <h3>OBJETIVO:</h3>
+                    <p class="my-3 text-justify">
+                    Crear un escenario híbrido para actualizarse en temas de vanguardia sobre
+                    administración y negocios, software aplicado a los negocios, gestión de talento
+                    humano, conocer resultados de investigaciones, compartir experiencias y
+                    prepararnos para los grandes retos del mundo post - pandemia.                
+                    </p>
+                    <div class="row">
+                        <div class="col-6">
+                            <img src="img/imgTecno_.jpg" class="img-fluid rounded wow animate__animated animate__fadeInLeft" alt="img Tecno 1">
+                        </div>
+                        <div class="col-6">
+                            <img src="img/imgTecno2_.jpg" class="img-fluid rounded wow animate__animated animate__fadeInUp" alt="img Tecno 2">
+                        </div>
+                    </div>
+    
+                </div>
+
+            </div>
+            <div class="row">
+                <div class="col-8 my-5 mx-auto">
+                    <h3>SEDE PRESENCIAL</h3>
+                    <p class="my-3 text-justify">
+                    UT Fidel Velázquez (Sede Presencial)               
+                    </p>
+                    <br><br>
+                    <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop" class="wow animate__animated animate__fadeIn rounded" class="mx-auto" style="width: 100%;">
+                        <source src="videos/videoInstalaciones.mp4" type="video/mp4">
+                    </video>   
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-8 my-5 mx-auto">
+                    <h3>SEDES VIRTUALES:</h3>
+                    <p class="my-3 text-justify">
+                    UT Valle del Mezquital
+                    <br>
+                    UT Nayarit
+                    <br>
+                    UT Zacatecas
+                    <br>
+                    UT Sureste del Estado de Veracruz
+                    <br>
+                    UT Riviera Maya
+                    <br>
+                    UT Calvillo              
+                    </p>
+                    <br><br>
+                    <div class="row">
+                        <div class="col-6">
+                            <img src="img/UTValle_del_Mezquital.jpg" class="img-fluid rounded mb-4 wow animate__animated animate__fadeInLeft" alt="UT Valle del Mezquital">
+                        </div>
+                        <div class="col-6">
+                            <img src="img/UTNayarit.jpg" class="img-fluid rounded mb-4 wow animate__animated animate__fadeInUp" alt="UT Nayarit">
+                        </div>
+                        <div class="col-6">
+                            <img src="img/UTZacatecas.jpg" class="img-fluid rounded mb-4 wow animate__animated animate__fadeInDown" alt="UT Zacatecas">
+                        </div>
+                        <div class="col-6">
+                            <img src="img/UTSuresteEstadoVeracruz.jpg" class="img-fluid rounded mb-4 wow animate__animated animate__fadeInLeft" alt="img Tecno 2">
+                        </div>
+                        <div class="col-6">
+                            <img src="img/UTRivieraMaya.jpg" class="img-fluid rounded mb-4 wow animate__animated animate__fadeInLeft" alt="img Tecno 1">
+                        </div>
+                        <div class="col-6">
+                            <img src="img/UTCalvillo.jpg" class="img-fluid rounded mb-4 wow animate__animated animate__fadeInUp" alt="UT Calvillo">
+                        </div>
+                    </div>  
+                </div>
+            </div>
+                        
         </div>
+        <div class="row">
+            <div class="col-12 m-0 p-0">
+                <img src="img/fondoBanner.jpg" class="img-fluid" alt="banner">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-8 my-5 mx-auto text-center">
+                <h3>Programa General de Actividades</h3>
+                <h4>Miércoles 16 de Noviembre de 2022</h4>
+                <img src="img/cal16Nov22.png" class="img-fluid mx-auto" alt="Calendario del día 16 de Noviembre 2022">  
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12 m-0 p-0">
+                <img src="img/fondoBanner0.jpg" class="img-fluid" alt="banner">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-8 my-5 mx-auto text-center">
+                <h3>Programa General de Actividades</h3>
+                <h4>Miércoles 17 de Noviembre de 2022</h4>
+                <img src="img/cal17Nov22.png" class="img-fluid mx-auto" alt="Calendario del día 17 de Noviembre 2022">  
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 m-0 p-0">
+                <img src="img/fondoBanner2.jpg" class="img-fluid" alt="banner">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-8 my-5 mx-auto text-center">
+                <h3>Programa General de Actividades</h3>
+                <h4>Miércoles 18 de Noviembre de 2022</h4>
+                <img src="img/cal18Nov22.png" class="img-fluid mx-auto" alt="Calendario del día 18 de Noviembre 2022">  
+            </div>
+        </div>
+
+        <br>
+        <br>
+        <div class="row">
+            <div class="col-8 my-5 mx-auto">
+                <div class="row">
+                    <div class="col-4">
+                        <h2>Costos</h2>
+                        <br>
+                        <p class="my-3 text-justify">
+                        Cuota de recuperación:
+                        <br>
+                        <b>Universidad = $4,000.00</b>
+                        <br>
+                        <br>
+                        <br>
+                        Presencial
+                        <br>
+                        <b>Estudiante = $200.00</b>
+                        <br>
+                        <b>Docente = $200.00</b>
+                        <br>
+                        <br>
+                        <br>
+                        Virtual
+                        <br>
+                        <b>Estudiante = $100.00</b>
+                        <br>
+                        <b>Docente = $100.00</b>
+                        <br>
+                        <br>
+                        <br>
+                        <b>Inscripción de Trabajos de Investigación: $1,500.00</b>
+                        </p>
+                        <br><br>
+                    </div>
+                    <div class="col-8 mx-auto text-center">
+                        <table class="mx-auto text-center">
+                            <tr>
+                                <td>
+                                    <img src="img/Logo.svg" class="img-fluid" alt="logo Video" style="width: 250px;">
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="img/videoConferencia.jpg" class="img-fluid rounded my-2" alt="logo Video" style="width: 250px;">
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <img src="img/videoConferencia2.jpg" class="img-fluid rounded my-2" alt="logo Video" style="width: 250px;">
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+        <!-- <div class="row">
+            <div class="col-12 m-0 p-0">
+                <header class="mt-3">                
+                    <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop" class="animate__animated animate__fadeIn" style="width: 300px;">
+                        <source src="videos/videoInstalaciones.mp4" type="video/mp4">
+                    </video>
+                </header>
+            </div>
+        </div> -->
     </div>
     <!-- <script src="js/jquery.min.js"></script> -->
     <!-- <script src="js/bootstrap.min.js"></script> -->
     <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script>
+        new WOW().init();
+    </script>
 </body>
 </html>
