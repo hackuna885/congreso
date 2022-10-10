c78398e60912b1b9f0334d0095d9245d81f136aa
hackuna hack <oliver885@hotmail.com>
2022-10-10 17:20:46 -0500

    10102022

---
+++ emprendimiento/index.php
@@ -0,0 +1,5 @@
+<html>
+	<head>
+		<meta http-equiv="REFRESH" content="0; url=https://forms.office.com/r/nN6CRRRGQ0">
+	</head>
+</html>
---
+++ simuladores/index.php
@@ -0,0 +1,5 @@
+<html>
+	<head>
+		<meta http-equiv="REFRESH" content="0; url=../pdf/BasesRetoLabsag_UT_2022_XVIII_CNAN.pdf">
+	</head>
+</html>
